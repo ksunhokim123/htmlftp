e05ef96f06e9ee792a0a41a012be55ad61a9c1ec
ksunhokim123 <ksunhokim123@gmail.com>
2018-02-12 16:11:17 +0900

    remove .fusebox

--- .fusebox/cache/3.1.3/F6gbH-app/static/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = { contents: "var name = \"FuseBox\";\nconsole.log(name);\n",
-dependencies: [],
-sourceMap: {},
-headerContent: undefined,
-mtime: 1518418847363,
-devLibsRequired : undefined,
-ac : undefined,
-_ : {}
-}
